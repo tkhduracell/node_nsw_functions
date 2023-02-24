ff19c7e30392d42b52e31582c9b73dcc72d7f2c8
Filip Lindqvist <buggfille@gmail.com>
2023-02-24 11:16:33 +0100

    New deploy

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ set -e
 tsc
 
 gcloud \
-    --project "${GCLOUD_PROJECT}" functions deploy nsw-calendar-export-get \
-    --runtime=nodejs16 \
+    --project "${GCLOUD_PROJECT}" functions deploy calendar-export-get \
+    --runtime=nodejs18 \
     --region="${GCLOUD_REGION}" \
     --source=. \
     --entry-point=get \
@@ -13,8 +13,8 @@ gcloud \
     --allow-unauthenticated
 
 gcloud \
-    --project "${GCLOUD_PROJECT}" functions deploy nsw-calendar-export-update \
-    --runtime=nodejs16 \
+    --project "${GCLOUD_PROJECT}" functions deploy calendar-export-update \
+    --runtime=nodejs18 \
     --region="${GCLOUD_REGION}" \
     --source=. \
     --entry-point=update \
@@ -22,12 +22,12 @@ gcloud \
     --memory=1024MB \
     --allow-unauthenticated
 
-gcloud scheduler jobs update http nsw-calendar-update \
+gcloud scheduler jobs update http calendar-update-half-hourly \
   --location "${GCLOUD_REGION}" \
-  --schedule "0 * * * *" \
+  --schedule "*/30 * * * *" \
   --time-zone "Europe/Stockholm" \
-  --uri "https://${GCLOUD_REGION}-${GCLOUD_PROJECT}.cloudfunctions.net/nsw-calendar-export-update" \
+  --uri "https://${GCLOUD_REGION}-${GCLOUD_PROJECT}.cloudfunctions.net/calendar-export-update" \
   --http-method GET \
-  --description="nsw-calendar-export-update: Update NSW GCS ics files"
+  --description="calendar-export-update: Update NSW GCS ics files (half-hourly)"
 
 rm ./*.js
