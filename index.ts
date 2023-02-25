a0a48d71a7759eb7bdeab45915a565d79c23df69
Filip Lindqvist <buggfille@gmail.com>
2023-02-25 21:35:40 +0100

    Add change detection

--- index.ts
+++ index.ts
@@ -5,6 +5,10 @@ import { Storage } from '@google-cloud/storage'
 import { launch, Page } from 'puppeteer'
 import { writeFile as _writeFile } from 'fs'
 import { promisify } from 'util'
+import { max, mapKeys } from 'lodash'
+import { differenceInMinutes, format, formatDistance, parseISO } from 'date-fns'
+import { sv } from 'date-fns/locale'
+
 import { initializeApp } from 'firebase-admin/app'
 import { getMessaging } from 'firebase-admin/messaging'
 
@@ -12,6 +16,7 @@ import fetch from 'cross-fetch'
 import z from 'zod'
 
 import {config} from 'dotenv'
+import { credential } from 'firebase-admin'
 
 config()
 
@@ -22,7 +27,7 @@ const {
     ACTIVITY_PASSWORD,
     GCLOUD_PROJECT,
     GCLOUD_BUCKET,
-    GCLOUD_REGION
+    GCLOUD_FUNCITON_GET_URL
 } = z.object({
     ACTIVITY_ORG_ID: z.string().min(1),
     ACTIVITY_BASE_URL: z.string().url(),
@@ -30,8 +35,10 @@ const {
     ACTIVITY_PASSWORD: z.string().min(1),
     GCLOUD_PROJECT: z.string().min(1),
     GCLOUD_BUCKET: z.string().min(1),
-    GCLOUD_REGION: z.string().min(1)
+    GCLOUD_FUNCITON_GET_URL: z.string().url().optional()
 }).parse(process.env)
+
+const app = initializeApp({ projectId: GCLOUD_PROJECT });
 
 const storage = new Storage({ projectId: GCLOUD_PROJECT });
 const bucket = storage.bucket(GCLOUD_BUCKET)
@@ -42,19 +49,22 @@ const writeFile = promisify(_writeFile)
 // when you make an HTTP request to the deployed function's endpoint.
 http('get', async (req, res) => {
     if (!req.query.id) {
-        const [file] = await bucket.file('all.json').download()
+        const [file] = await bucket.file('index.json').download()
 
-        return res.header('content-type', 'application/json').send(file).end()
+        return res.header('Content-Type', 'application/json').send(file).end()
     }
 
     const id = z.string().regex(/\d+/).parse(req.query.id)
 
+    const [{metadata}] = await bucket.file(`cal_${id}.ics`).getMetadata()
+
     res
-        .setHeader('content-type', 'text/calendar')
+        .setHeader('Content-Type', 'text/calendar')
+        .setHeader('Content-Disposition', `attachment; filename="${metadata['CalendarName']}.ics"`)
         .status(200)
 
     bucket
-        .file(`${id}.ics`)
+        .file(`cal_${id}.ics`)
         .createReadStream()
         .pipe(res, { end: true })
 });
@@ -115,6 +125,17 @@ async function calendars(page: Page) {
 }
 
 export async function calendar(headless = true, useCGS = false) {
+
+    const out = await getMessaging().send({ // Try dl fb svc key and use it locally.
+        notification: {
+            title: 'Ny friträning inlagd',
+            body: '2023-05-31 kl 01:00 (ungefär två timmar) Filip test 2'
+        },
+        topic: 'cal-337667'
+      }, true)
+    console.log(out)
+    if (out) return;
+
     const browser = await launch({ headless });
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(1000 * 60 * 5);
@@ -139,6 +160,7 @@ export async function calendar(headless = true, useCGS = false) {
         const lastquater = new Date(today.getTime() - 1000 * 3600 * 24 * 90)
         const inayear = new Date(today.getTime() + 1000 * 3600 * 24 * 366)
 
+        const file = `/tmp/cal_${cal.id}.ics`
         const start = `${lastquater.toISOString().replace(/(.*)T.*/, '$1')}+00%3A00%3A00`
         const end = `${inayear.toISOString().replace(/(.*)T.*/, '$1')}+00%3A00%3A00`
 
@@ -147,33 +169,70 @@ export async function calendar(headless = true, useCGS = false) {
             method: 'GET',
             headers: { cookie }
         })
-        const text = postprocess(await response.text(), cal)
+        const { text, latest_date, latest_uid } = postprocess(await response.text(), cal)
+        console.log(`Processed - ${cal.name}`, {latest_date, latest_uid})
 
-        const file = `/tmp/${cal.id}.ics`
-
-        console.log(`Saving - ${cal.name} (${cal.id})`)
+        console.log(`Saving - ${cal.name}`)
         await writeFile(file, text)
-        console.log(`Wrote - ${cal.name} (${cal.id}) to ${file}`)
+        console.log(`Wrote -  ${cal.name} to ${file}`)
 
-        if(useCGS) {
-            const destination = `${cal.id}.ics`
+        const object = bucket.file(`cal_${cal.id}.ics`)
+        const [{ metadata }] = await object.exists() ? await object.getMetadata() : [{ metadata: {} }]
+        console.log({ metadata })
+        if ((latest_uid ?? '') > (metadata['calendar_last_uid'] ?? '')) {
+
+            const event = [...text.matchAll(/BEGIN:VEVENT[\s\S]+?UID:(\d+)[\s\S]+?END:VEVENT/ig)]
+                .map(s => s[0])
+                .filter(evt => evt.includes(`UID:${latest_uid}`))
+                .find(() => true)
+
+            const start = (event ?? '').match(/DTSTART:(.*)/)?.reverse().map(s => parseISO(s)).find(() => true)
+            const end = (event ?? '').match(/DTEND:(.*)/)?.reverse().map(s => parseISO(s)).find(() => true)
+            const summary = (event ?? '').match(/SUMMARY:(.*)/)?.reverse().find(() => true)
+            const duration = end && start ? differenceInMinutes(end, start) : null
+            console.log('New event discovered!', { start, end, duration, summary })
+
+            const topicName = `calendar-${cal.id}`;
+
+            const message = {
+              notification: {
+                title: cal.name === 'Friträning' ?  'Ny friträning inlagd' : `${cal.name} uppdaterad`,
+                body: start && end ? `${format(start, 'yyyy-MM-dd')} kl ${format(start, 'HH:mm')} (${formatDistance(start, end, { locale: sv })}) ${summary}` : summary
+              },
+              topic: topicName,
+            }
+            console.log({message})
+            const resp = await getMessaging(app).send(message)
+        }
+
+        if (useCGS) {
+            const destination = `cal_${cal.id}.ics`
             const metadata = {
-                'x-goog-meta-calendar-name': cal.name,
-                'x-goog-meta-calendar-id': cal.id
+                metadata: {
+                    'calendar_name': cal.name,
+                    'calendar_id': cal.id,
+                    'calendar_last_uid': latest_uid,
+                    'calendar_last_data': latest_date,
+                    'calendar_self': GCLOUD_FUNCITON_GET_URL ? `webcal://${GCLOUD_FUNCITON_GET_URL}?id=${cal.id}` : undefined
+                }
             }
             console.log(`Uploading - ${cal.name} (${cal.id}) to ${bucket.cloudStorageURI}/${destination}`)
-            await bucket.upload(file, { destination, metadata: { customMetadata: metadata } })
+            await bucket.upload(file, { destination, metadata })
         }
     }
 
     await browser.close();
 
     if (useCGS) {
-        const data = cals.map(cal => ({...cal, self: `webcal://${GCLOUD_REGION}-${GCLOUD_PROJECT}.cloudfunctions.net/nsw-calendar-export-get?id=${cal.id}` }))
+        const [files] = await bucket.getFiles({ prefix: 'cal_' })
+
+        const metadatas = await Promise.all(files.map(f => f.getMetadata()))
+        const data = metadatas.map(([{ metadata }]) => mapKeys(metadata, (_, k) => k.replace('calendar_', '')))
         const payload = JSON.stringify(data, null, 2)
-        const file = `/tmp/all.json`
+
+        const file = `/tmp/index.json`
         await writeFile(file, payload)
-        await bucket.upload(file, { destination: 'all.json' })
+        await bucket.upload(file, { destination: 'index.json' })
     }
 }
 
@@ -181,10 +240,13 @@ function sleep(ms: number = 20000) {
     return new Promise((res, rej) => setTimeout(res, ms))
 }
 
-function postprocess(text: string, calendar: { name: string }) {
+function postprocess(content: string, calendar: { name: string }) {
     const new_fields = [
         'URL:https://nackswinget.se/Kalender',
         `NAME:${calendar.name}`,
     ]
-    return text.replace(/(X-WR-CALNAME):.*/gi, `\$1:${calendar.name}\n${new_fields.join('\n')}`)
+    const text = content.replace(/(X-WR-CALNAME):.*/gi, `\$1:${calendar.name}\n${new_fields.join('\n')}`)
+    const latest_date = max([...text.matchAll(/DTSTAMP:.*/gi)].map(s => s[0].replace('DTSTAMP:', ''))) ?? null
+    const latest_uid = max([...text.matchAll(/UID:.*/gi)].map(s => s[0].replace('UID:', ''))) ?? null
+    return { text, latest_date: latest_date ? parseISO(latest_date) : null, latest_uid }
 }
