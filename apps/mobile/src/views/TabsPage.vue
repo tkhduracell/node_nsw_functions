10ac9ef1826ba0526aa9d1cda9fd443fccdfdde3
Filip Lindqvist <buggfille@gmail.com>
2024-01-23 22:58:12 +0100

    Initial commit

---
+++ apps/mobile/src/views/TabsPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+
+        <ion-tab-button tab="tab1" href="/tabs/open-practice">
+          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-label>Friträningar</ion-label>
+        </ion-tab-button>
+
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { triangle } from 'ionicons/icons';
+</script>
