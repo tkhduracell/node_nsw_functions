363f3c51aec24f8e24c92f2f6f87aed3b3185cdb
Filip Lindqvist <buggfille@gmail.com>
2024-01-28 11:37:52 +0100

    Update

--- apps/mobile/capacitor.config.ts
+++ apps/mobile/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'org.nackswinget.apps',
   appName: 'mobile',
   webDir: 'dist',
   server: {
