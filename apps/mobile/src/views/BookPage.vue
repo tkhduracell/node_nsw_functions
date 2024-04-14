c4cebd143a3fb325376b26494f73087c60c2acf2
Filip Lindqvist <buggfille@gmail.com>
2024-04-14 19:59:53 +0200

    Add booking handling when using booking frame

--- apps/mobile/src/views/BookPage.vue
+++ apps/mobile/src/views/BookPage.vue
@@ -1,7 +1,8 @@
 <!-- eslint-disable vue/no-v-text-v-html-on-component -->
 <template>
   <ion-page>
-    <iframe src="https://nackswinget.se/boka" class="w-100 h-100" />
+    <iframe src="https://europe-north1-nackswinget-af7ef.cloudfunctions.net/calendars-api/book" class="w-100 h-100"
+      :onLoad="onLoaded" ref="iframe" />
   </ion-page>
 </template>
 
@@ -15,7 +16,39 @@
 }
 </style>
 <script setup lang="ts">
+
 import {
   IonPage
 } from '@ionic/vue';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { Toast } from '@capacitor/toast';
+
+const initallyLoaded = ref(false);
+const iframe = ref<HTMLIFrameElement | null>(null);
+
+const router = useRouter();
+
+async function onLoaded() {
+  if (initallyLoaded.value) {
+    await Toast.show({
+      text: 'Bokningen slutfördes. Det tar några minutier innan den syns i kalendern.',
+      duration: 'long'
+    });
+    await router.push('/tabs/calendar')
+      .finally(() => {
+        const element = iframe.value;
+        initallyLoaded.value = false
+        if (element) {
+          element.src = "https://europe-north1-nackswinget-af7ef.cloudfunctions.net/calendars-api/book?t=" + new Date().getTime();
+          setTimeout(() => initallyLoaded.value = false, 1000)
+        }
+      });
+  }
+
+  initallyLoaded.value = true;
+}
+onMounted(() => {
+  initallyLoaded.value = false;
+});
 </script>
