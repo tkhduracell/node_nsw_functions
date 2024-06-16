3c670562afc692d0d8d354d9a5264d03b8503412
Filip Lindqvist <buggfille@gmail.com>
2024-06-16 16:41:13 +0200

    chore: Add LAN address

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -3,7 +3,7 @@ import setupCors, { CorsOptions } from 'cors';
 export const ALLOWED_ORIGINS = [
     'capacitor://localhost',
     'ionic://localhost',
-    'http://localhost',
+    'http://192.168.68.57:3000',
     'http://localhost:5173',
     'http://localhost:8080',
     'https://localhost',
