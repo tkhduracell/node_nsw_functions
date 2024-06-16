c8af8a1ca707717eccfc581f99edf0769a04e649
Filip Lindqvist <buggfille@gmail.com>
2024-06-16 16:42:22 +0200

    fix: iOS/Desktop adjust bookingiframe

--- apps/mobile/src/views/BookPage.vue
+++ apps/mobile/src/views/BookPage.vue
@@ -2,7 +2,7 @@
 <template>
   <ion-page>
     <iframe src="https://europe-north1-nackswinget-af7ef.cloudfunctions.net/calendars-api/book" class="w-100 h-100"
-      :onLoad="onLoaded" ref="iframe" />
+      :onLoad="onLoaded" ref="iframe" :style="{ paddingTop: isIOS ? '3em' : '0' }" />
   </ion-page>
 </template>
 
@@ -18,7 +18,7 @@
 <script setup lang="ts">
 
 import {
-  IonPage
+  IonPage, isPlatform
 } from '@ionic/vue';
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
@@ -26,21 +26,28 @@ import { Toast } from '@capacitor/toast';
 
 const initallyLoaded = ref(false);
 const iframe = ref<HTMLIFrameElement | null>(null);
-
+const isIOS = isPlatform('ios');
+const isDesktop = isPlatform('desktop');
 const router = useRouter();
 
 async function onLoaded() {
   if (initallyLoaded.value) {
-    await Toast.show({
-      text: 'Bokningen slutfördes. Det tar några minutier innan den syns i kalendern.',
-      duration: 'long'
-    });
+    if (isDesktop) {
+      console.log('Bokningen slutfördes.', { initallyLoaded: initallyLoaded.value });
+    } else {
+      Toast.show({
+        text: 'Bokningen slutfördes. Det tar några minuter innan den syns i kalendern.',
+        duration: 'long'
+      });
+    }
     await router.push('/tabs/calendar')
       .finally(() => {
         const element = iframe.value;
         initallyLoaded.value = false
         if (element) {
-          element.src = "https://europe-north1-nackswinget-af7ef.cloudfunctions.net/calendars-api/book?t=" + new Date().getTime();
+          const url = "https://europe-north1-nackswinget-af7ef.cloudfunctions.net/calendars-api/book?t=" + new Date().getTime()
+          console.log('Reloading iframe', { url });
+          element.src = url;
           setTimeout(() => initallyLoaded.value = false, 1000)
         }
       });
