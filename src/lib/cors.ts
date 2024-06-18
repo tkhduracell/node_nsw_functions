93868de5c9eb62517d27580e3cb3968bd2d7e69e
Filip Lindqvist <buggfille@gmail.com>
2024-06-18 20:54:47 +0200

    Update cors.ts

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -3,7 +3,7 @@ import setupCors, { CorsOptions } from 'cors';
 export const ALLOWED_ORIGINS = [
     'capacitor://localhost',
     'ionic://localhost',
-    'http://192.168.68.57:3000',
+    'http://192.168.68.54:3000',
     'http://localhost:5173',
     'http://localhost:8080',
     'https://localhost',
