26a18cc02f301e7aaa78c484a072a925450d8528
Filip Lindqvist <buggfille@gmail.com>
2024-12-10 09:36:02 +0100

    Format jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: [
-    "<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}",
-    "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
-  ],
-  setupFiles: ["dotenv/config"],
-};
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [
+        '<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}',
+        '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}'
+    ],
+    setupFiles: ['dotenv/config'],
+}
